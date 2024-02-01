81b99bf1d16e4ca3e5d04f212da6d2a8512b25fb
Nicolas Romero <Kruznicolas@gmail.com>
2024-02-01 05:49:11 -0500

    README.md fixed

--- Backend/src/models/carts.models.js
+++ Backend/src/models/carts.models.js
@@ -6,7 +6,6 @@ const collection = "carts";
 
 const schema = new mongoose.Schema(
   {
-    // products: [ productId:{ type: mongoose.Schema.Types.ObjectId, ref:'products'}, quantity:{ type: Number, default: 1}, _id: {require: false}],
     products: [
       {
         _id: false,
@@ -14,9 +13,6 @@ const schema = new mongoose.Schema(
         quantity: { type: Number, default: 1 },
       },
     ],
-    // products: { type: [mongoose.Schema.Types.ObjectId], ref:'products'},
-    // products: [ {productId: {type: String, require: true}, quantity: {type: Number, default: 1}}],
-    //total: {type: Number, require: true}
   },
   {
     versionKey: false,
